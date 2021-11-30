ffcd222bfeefa73dfaec7eb1d8f49ffaafdb9db2
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-30 13:40:13 -0500

    css styling added to kennel and animal

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Kennel.css";
+import { AnimalCard } from "./animal/AnimalCard";
 
 export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
+        
         <address>
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
     </>
 )
