86f5622087f16c8e7dad756f33d5f603204d357e
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-01 14:17:07 -0500

    rendering list of animals to page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,25 +1,23 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
-import { Location } from "./location/Location"
-import {Customer} from "./customer/Customer"
-import {Employee} from "./employee/Employee"
+// import { AnimalCard } from "./animal/AnimalCard"
+// import { Location } from "./location/Location"
+// import {Customer} from "./customer/Customer"
+// import {Employee} from "./employee/Employee"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
 
 export const ApplicationViews = () => {
     return (
-        <Routes>
-            {/* Render the location list when http://localhost:3000/ */}
-            <Route path="/" element={<Home />} />
-             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="animals/*" element={<AnimalCard />} />
-            {/* Render the location list when http://localhost:3000/locations */}
-            <Route path="locations/*" element={<Location/>} />
-            {/* Render the customer list when http://localhost:3000/customers */}
-            <Route path="customers/*" element={<Customer />} />
-            {/* Render the employee list when http://localhost:3000/employees */}
-            <Route path="employees/*" element={<Employee />} />
-        </Routes>
+        <AnimalProvider>
+            <Routes>
+                {/* Render the location list when http://localhost:3000/ */}
+                <Route path="/" element={<Home />} />
+                {/* Render the animal list when http://localhost:3000/animals */}
+                <Route path="animals/*" element={<AnimalList />} />
+            </Routes>
+        </AnimalProvider>
     )
 }
 
