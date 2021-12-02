9706813902502e03dbc5743af5087be908dd557b
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-02 11:18:33 -0500

    employee components created, employee provider added to application views with respective routes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,19 +8,25 @@ import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
 import {LocationProvider} from "./location/LocationProvider"
 import {LocationList} from "./location/LocationList"
+import { EmployeeProvider } from "./employee/EmployeeProvider"
+import { EmployeeList } from "./employee/EmployeeList"
 
 export const ApplicationViews = () => {
     return (
         <AnimalProvider>
             <LocationProvider>
-                <Routes>
-                    {/* Render the location list when http://localhost:3000/ */}
-                    <Route path="/" element={<Home />} />
-                    {/* Render the animal list when http://localhost:3000/animals */}
-                    <Route path="animals/*" element={<AnimalList />} />
-                    {/* Render the location list when http://localhost:3000/locations */}
-                    <Route path="locations/*" element={<LocationList />} />
-                </Routes>
+                <EmployeeProvider>
+                    <Routes>
+                        {/* Render the location list when http://localhost:3000/ */}
+                        <Route path="/" element={<Home />} />
+                        {/* Render the animal list when http://localhost:3000/animals */}
+                        <Route path="animals/*" element={<AnimalList />} />
+                        {/* Render the location list when http://localhost:3000/locations */}
+                        <Route path="locations/*" element={<LocationList />} />
+                        {/* Render the employee list when http://localhost:3000/locations */}
+                        <Route path="employees/*" element={<EmployeeList />} />
+                    </Routes>
+                </EmployeeProvider>
             </LocationProvider>
         </AnimalProvider>
     )
