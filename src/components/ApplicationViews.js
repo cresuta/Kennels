7a710372ca2d8159204ec7df004d9e800f11c990
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-02 11:10:48 -0500

    location provider in application view with respective routes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,22 +1,27 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
-// import { AnimalCard } from "./animal/AnimalCard"
 // import { Location } from "./location/Location"
 // import {Customer} from "./customer/Customer"
 // import {Employee} from "./employee/Employee"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
+import {LocationProvider} from "./location/LocationProvider"
+import {LocationList} from "./location/LocationList"
 
 export const ApplicationViews = () => {
     return (
         <AnimalProvider>
-            <Routes>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route path="/" element={<Home />} />
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route path="animals/*" element={<AnimalList />} />
-            </Routes>
+            <LocationProvider>
+                <Routes>
+                    {/* Render the location list when http://localhost:3000/ */}
+                    <Route path="/" element={<Home />} />
+                    {/* Render the animal list when http://localhost:3000/animals */}
+                    <Route path="animals/*" element={<AnimalList />} />
+                    {/* Render the location list when http://localhost:3000/locations */}
+                    <Route path="locations/*" element={<LocationList />} />
+                </Routes>
+            </LocationProvider>
         </AnimalProvider>
     )
 }
