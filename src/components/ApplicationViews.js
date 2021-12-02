70c05d128b2bbb8894c0ab17b2c4c8a41c77867d
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-02 11:25:32 -0500

    customer components created, customer provider added to application views with respective routes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,31 +1,34 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./Home"
-// import { Location } from "./location/Location"
-// import {Customer} from "./customer/Customer"
-// import {Employee} from "./employee/Employee"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
 import {LocationProvider} from "./location/LocationProvider"
 import {LocationList} from "./location/LocationList"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import { EmployeeList } from "./employee/EmployeeList"
+import { CustomerProvider } from "./customer/CustomerProvider"
+import { CustomerList } from "./customer/CustomerList"
 
 export const ApplicationViews = () => {
     return (
         <AnimalProvider>
             <LocationProvider>
                 <EmployeeProvider>
-                    <Routes>
-                        {/* Render the location list when http://localhost:3000/ */}
-                        <Route path="/" element={<Home />} />
-                        {/* Render the animal list when http://localhost:3000/animals */}
-                        <Route path="animals/*" element={<AnimalList />} />
-                        {/* Render the location list when http://localhost:3000/locations */}
-                        <Route path="locations/*" element={<LocationList />} />
-                        {/* Render the employee list when http://localhost:3000/locations */}
-                        <Route path="employees/*" element={<EmployeeList />} />
-                    </Routes>
+                    <CustomerProvider>
+                        <Routes>
+                            {/* Render the location list when http://localhost:3000/ */}
+                            <Route path="/" element={<Home />} />
+                            {/* Render the animal list when http://localhost:3000/animals */}
+                            <Route path="animals/*" element={<AnimalList />} />
+                            {/* Render the location list when http://localhost:3000/locations */}
+                            <Route path="locations/*" element={<LocationList />} />
+                            {/* Render the employee list when http://localhost:3000/locations */}
+                            <Route path="employees/*" element={<EmployeeList />} />
+                            {/* Render the customer list when http://localhost:3000/customers */}
+                            <Route path="customers/*" element={<CustomerList />} />
+                        </Routes>
+                    </CustomerProvider>
                 </EmployeeProvider>
             </LocationProvider>
         </AnimalProvider>
