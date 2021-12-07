c122756da73598de9c1da3bf82f824ae0587f08d
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-07 09:36:30 -0500

    refactoring animal list to only show hyperlinked animal name with breed, reservation button with form changed as well

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./Animal.css"
+import { Link } from "react-router-dom"
 
-export const AnimalCard = ({animal,customer,location}) => (
+export const AnimalCard = ({animal}) => (
     <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
-        <address className="location__address">Location: {location.name}</address>
-        <h4 className="customer__name">Customer: {customer.name}</h4>
+        <h3 className="animal__name">
+            <Link to={`/animals/detail/${animal.id}`}>
+                {animal.name}
+            </Link>
+        </h3>
+       <div className="animal__breed">{animal.breed}</div>
     </section>
 )
  
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,43 +1,29 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
-import { LocationContext } from "../location/LocationProvider"
-import {CustomerContext} from "../customer/CustomerProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 import { useNavigate } from "react-router"
 
-export const AnimalList = () => {
+export const AnimalList = ({history}) => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
-  const {locations, getLocations} = useContext(LocationContext)
-  const { customers, getCustomers } = useContext(CustomerContext)
   const navigate = useNavigate()
 
 
   //useEffect - reach out to the world for something
   useEffect(() => {
-    console.log("AnimalList: useEffect - getAnimals")
-    getLocations()
-    .then(getCustomers)
-    .then(getAnimals)
-
+   getAnimals()
   }, [])
 
 
   return (
     <>
     <h2>Animals</h2>
-    <button onClick={() => {navigate("create")}}>Add Animal</button>
+    <button onClick={() => {navigate("/animals/create")}}>Make Reservation</button>
     <div className="animals">
-      {console.log("AnimalList: Render", animals)}
       {
         animals.map(animal => {
-          const owner = customers.find(c => c.id === animal.customerId)
-          const clinic = locations.find(l => l.id === animal.locationId)
-          return <AnimalCard key={animal.id} 
-          animal={animal}
-          customer={owner}
-          location={clinic} />
+          return <AnimalCard key={animal.id} animal={animal} />
         })
       }
     </div>
